466b031df5a4c3bdee56e89d0926d12e0fb1cd03
Will Seese <willxj36@gmail.com>
2024-11-07 11:33:45 -0600

    initial commit with new config

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,6 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
-const serverConfig = {
-	mode: process.env.NODE_ENV || 'development',
-	entry: './src/server/server.ts',
-	module: {
-		rules: [
-			{
-				test: /\.ts?$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/,
-				options: {
-					configFile: 'tsconfig.server.json'
-				}
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.ts', '.js']
-	},
-	output: {
-		filename: 'server.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	target: 'node',
-	node: {
-		__dirname: false
-	},
-	externals: [nodeExternals()]
-};
-
 const clientConfig = {
 	mode: process.env.NODE_ENV || 'development',
 	entry: './src/client/index.tsx',
@@ -59,4 +30,4 @@ const clientConfig = {
 	}
 };
 
-module.exports = [serverConfig, clientConfig];
+module.exports = [clientConfig];
